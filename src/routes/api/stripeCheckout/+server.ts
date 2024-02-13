c808f89aff2224fa2f12a92815d03c4098fbdb46
Gamzee <lithiumturkin@protonmail.com>
2024-02-13 12:11:41 +0100

    Feat: add stripe and stripe backend

---
+++ src/routes/api/stripeCheckout/+server.ts
@@ -0,0 +1,36 @@
+import type { RequestHandler } from "./$types";
+import Stripe from 'stripe'
+
+const SECRET_STRIPE_KEY = "";
+const stripe = new Stripe(SECRET_STRIPE_KEY, {
+    apiVersion: "2023-10-16"
+})
+
+export const POST: RequestHandler = async ({request}) => {
+    const data = await request.json();
+    const items = data.items;
+
+    //change array to valid array stripe expects
+    const lineItems : any = [];
+    items.array.forEach((item: any) => {
+        lineItems.push( {price: item.id, quantity: item.quantity} )
+    });
+
+
+    //gives a unique url for the person to check out with
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: '/success',
+        cancel_url: '/cancel',
+    })
+
+    //send the url to the frontend
+    return new Response(
+        JSON.stringify({ url: session.url }),
+        {
+            status: 200,
+            headers: {'content-type': 'application/json'}
+        }
+    );
+}
