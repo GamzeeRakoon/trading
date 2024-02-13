e4bd03e943b27ad0e481e456cef4d897d94ddb43
Gamzee <lithiumturkin@protonmail.com>
2024-02-13 12:11:26 +0100

    Feat: add cart system

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -1,18 +1,37 @@
-<script>
+<script lang="ts">
+  import { get } from "svelte/store";
+  import { cartItems, addToCart, removeFromCart } from "../stores/cart";
+  export let product : Product = {id: "", name: "", price: 0};
 
+  // get writable and match id from cart and from the product
+  let cart = get(cartItems);
+  let cartItemIndex = cart.findIndex((item) => { return item.id === product.id });
+  let cartProduct = cart[cartItemIndex]
+  //check for changes in the cart via susbcribe
+  cartItems.subscribe((newCartValue) => {
+    cart = newCartValue;
+    cartItemIndex = cart.findIndex((item) => { return item.id === product.id });
+    cartProduct = cart[cartItemIndex]
+  })
+
+  console.log(product)
 </script>
 
-<div class="card w-96 bg-base-200 shadow-xl">
+<div class="w-96 shadow-xl card bg-base-200">
     <div class="card-body">
-      <h2 class="card-title">Product Store</h2>
+      <h2 class="card-title">{product.name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
       <div class="font-bold">
-        <p>Quantity: 40</p>
-        <p>Price: $2</p>
+        {#if cartProduct !== undefined}
+          <p>Quantity: {cartProduct.quantity}</p>
+        {:else}
+          <p>Quantity: 0</p>
+        {/if}
+        <p>Price: ${product.price}</p>
       </div>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Add</button>
-        <button class="btn btn-error">Remove</button>
+      <div class="justify-end card-actions">
+        <button class="btn btn-primary" on:click={() => addToCart(product.id)}>Add</button>
+        <button class="btn btn-error" on:click={() => removeFromCart(product.id)}>Remove</button>
       </div>
     </div>
   </div>
---
+++ src/stores/cart.ts
@@ -0,0 +1,61 @@
+import { writable, get } from "svelte/store";
+
+export const cartItems = writable<CartItem[]>([]);
+
+export const addToCart = (id: string) => {
+  const items = get(cartItems);
+  //does current item have an id of 1 or higher
+  const itemPosition = items.findIndex(
+    (item) => { return item.id == id}
+  );
+
+  //if quantity isnt lower then -1 add to quantity
+  if( itemPosition !== -1 ) {
+    //item has quantity, add +1 to quantity
+    cartItems.update(() => {
+      const updatedItems = items.map( (item) => {
+
+        if(item.id === id) {
+          return {...item, quantity: item.quantity + 1}
+        }
+
+        return item;
+      });
+
+      return updatedItems;
+    });
+  } else {
+    //item isnt in cart, add object by setting quantity to 1
+    cartItems.update(() => {
+      return [...items, {id: id, quantity: 1}]
+    });
+  }
+}
+
+export const removeFromCart = (id: string) => {
+  const items = get(cartItems);
+
+  //does current item have an id of 1 or higher
+  const itemPosition = items.findIndex(
+    (item) => { return item.id == id}
+  );
+
+  //if item quantity -1 === 0 delete item from cart
+  if(items[itemPosition]?.quantity - 1 === 0) {
+    items.splice(itemPosition, 1);
+  }
+
+  //item has quantity, remove -1 of quantity
+  cartItems.update(() => {
+    const updatedItems = items.map( (item) => {
+
+      if(item.id === id) {
+        return {...item, quantity: item.quantity - 1}
+      }
+
+      return item;
+    });
+
+    return updatedItems;
+  });
+}
