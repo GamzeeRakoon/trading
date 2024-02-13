49e13b50dbb9d70199e33ed1e66d6281a11f546b
Gamzee <lithiumturkin@protonmail.com>
2024-02-13 12:10:52 +0100

    Chore: rename route and add cart svg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,36 +12,36 @@
     }
 
 </script>
-  <Navbar />
+<Navbar />
 
-  <main class="flex flex-col">
-      <div class="hero">
-          <div class="flex-col hero-content">
-            <div class="text-center lg:text-left">
-              <h1 class="text-5xl font-bold">Login Simulation</h1>
-              <p class="py-6 text-center">This is a simulated login screen!</p>
-            </div>
-            <div class="w-full max-w-sm shadow-2xl card shrink-0 bg-base-200">
-              <form class="card-body">
-                <div class="form-control">
-                  <select class="w-full max-w-xs select select-bordered" bind:value={selectedUser} on:change={handleUserChange}>
-                      <option disabled selected>Select User</option>
-                      <option>Admin</option>
-                      <option>User</option>
-                    </select>
-                </div>
-                <div class="mt-6 form-control">
-                    {#if isLoginDisabled === true}
-                      <button class="btn btn-primary" disabled>Login</button>
-                    {:else}
-                      <a class="btn btn-primary" href="/store">Login</a>
-                    {/if}
-
-                </div>
-              </form>
-            </div>
+<main class="flex flex-col">
+    <div class="hero">
+        <div class="flex-col hero-content">
+          <div class="text-center lg:text-left">
+            <h1 class="text-5xl font-bold">Login Simulation</h1>
+            <p class="py-6 text-center">This is a simulated login screen!</p>
           </div>
-      </div>
-  </main>
+          <div class="w-full max-w-sm shadow-2xl card shrink-0 bg-base-200">
+            <form class="card-body">
+              <div class="form-control">
+                <select class="w-full max-w-xs select select-bordered" bind:value={selectedUser} on:change={handleUserChange}>
+                    <option disabled selected>Select User</option>
+                    <option>Admin</option>
+                    <option>User</option>
+                  </select>
+              </div>
+              <div class="mt-6 form-control">
+                  {#if isLoginDisabled === true}
+                    <button class="btn btn-primary" disabled>Login</button>
+                  {:else}
+                    <a class="btn btn-primary" href="/products">Login</a>
+                  {/if}
 
-  <Footer />
+              </div>
+            </form>
+          </div>
+        </div>
+    </div>
+</main>
+
+<Footer />
